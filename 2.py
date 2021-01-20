b4847e441015546fc4b44321a786b8e1c480510b
min-0 <dhe08133@naver.com>
2021-01-20 09:23:51 +0900

    second

--- 2.py
+++ 2.py
@@ -1,27 +1,33 @@
-
-# 목표는, 멤버들 블로그 전체 게시글 가져오기
-# 아이디를 넣으면, 그 아이디의 블로그 전체 게시글 제목들을 가져오는 함수 구현
-
- #0번째 member만 가져올 수 있음, 1번째도 가져와야 함
-
 import requests
 import json
 from bs4 import BeautifulSoup
 
 def get_blog_posts(id):
+
+    print("<" + id + ">" + " 을(를) 파헤치자")
+
     url = data['tistory-left'] + id + data['tistory-right']
     req = requests.get(url)
-    
+
     soup = BeautifulSoup(req.content, 'html.parser')
 
     res = soup.select('#mArticle > div > a.link_post > strong')
 
-    for i in res:
-        print(i.contents[0])
-      
+    if len(res) == 0:
+        print("nothing")
+    else:
+        for i in res:
+            print(i.contents[0])
+
+def blogpost_scraping(data):
+
+    for i in data['member']:
+        get_blog_posts(i)
+
+
 if __name__ == "__main__":
-    
+
     with open('./data.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
-    
-    get_blog_posts(data['member'][0])
+
+    blogpost_scraping(data)
